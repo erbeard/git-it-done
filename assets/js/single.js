6eedbcb536bb7844f0e2e3a49714b5dd4d7fdd9f
Ed Beard <ed.r.beard@gmail.com>
2022-09-05 11:14:32 -0400

    add language buttons

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,13 +2,10 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
-
 var getRepoName = function() {
   // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  getRepoIssues(repoName);
-  repoNameEl.textContent = repoName;
 
   if (repoName) {
     // display repo name on the page
@@ -19,8 +16,6 @@ var getRepoName = function() {
     // if no repo was given, redirect to the homepage
     document.location.replace("./index.html");
   }
-
-
 };
 
 var getRepoIssues = function(repo) {
@@ -39,10 +34,9 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      c// if not successful, redirect to homepage
-    document.location.replace("./index.html");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -60,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -75,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
